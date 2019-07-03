b70c5ada5c6fb59f0ea50e74473c0dfd31311fce
stevenhorsman <steven@uk.ibm.com>
2019-07-03 15:10:58 +0100

    Fix typo
    Signed-off-by: stevenhorsman <steven@uk.ibm.com>

--- .pax/post-packaging.sh
+++ .pax/post-packaging.sh
@@ -53,7 +53,7 @@ mkdir -p zowe
 
 pwd
 
-run ./bld/smpe.sh -i \"${PWD}/${INPUT_TXT}\" -v 120 -r \"${PWD}/zowe\""
+run ./bld/smpe.sh -i \"${PWD}/${INPUT_TXT}\" -v 120 -r \"${PWD}/zowe\"
 
 # exit for manual processing
 #exit 1
